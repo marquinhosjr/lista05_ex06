a4bdfd2f29436191f8bad6738cd83cdf50e2328f
Marcos Masquetti <marquinhosmasquettij@hotmail.com>
2022-12-08 19:10:21 -0300

    exercicio 6

---
+++ lista05_ex06/src/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+    
+        System.out.print("Digite as horas: ");
+        int horas = sc.nextInt();
+        System.out.print("Digite os minutos: ");
+        int minutos = sc.nextInt();
+        sc.close();
+
+        if(horas > 12) {
+            horas = horas - 12;
+            System.out.print(horas + ":" + minutos + " P.M.");
+        }
+        else {
+            System.out.print(horas + ":" + minutos + " A.M.");
+        }
+    }
+}
